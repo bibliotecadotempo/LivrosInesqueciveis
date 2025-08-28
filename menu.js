3fcbabfa28cd0dcbdd936bb7ac679cc7f9c7e2a7
João Baptista Alves Ferreira Filho <bibliotecadotempo2025@gmail.com>
2025-08-28 17:16:09 -0300

    atualizar

---
+++ menu.js
@@ -0,0 +1,6 @@
+fetch("menu.html")
+  .then(response => response.text())
+  .then(data => {
+    document.getElementById("menu").innerHTML = data;
+  })
+  .catch(error => console.error("Erro ao carregar o menu:", error));
